89407b075b5b56dc5de9c37527e7cba7a4340782
Julian Diaz <alexdior59@gmail.com>
2025-09-30 14:46:17 -0500

    Add CrossOrigin annotation to UsuarioController for localhost:4200

--- Proyecto1/src/main/java/com/example/proyecto1/Controller/UsuarioController.java
+++ Proyecto1/src/main/java/com/example/proyecto1/Controller/UsuarioController.java
@@ -13,6 +13,7 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/usuarios")
+@CrossOrigin(origins = "http://localhost:4200")
 public class UsuarioController {
 
     private final UsuarioService usuarioService;
